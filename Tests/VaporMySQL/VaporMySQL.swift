11b19df789483b7d7a1aadacd8358847d26d5580
Tanner Nelson <me@tanner.xyz>
2016-07-14 17:57:57 -0400

    test case update

--- Tests/VaporMySQL/VaporMySQL.swift
+++ Tests/VaporMySQL/VaporMySQL.swift
@@ -2,5 +2,11 @@ import XCTest
 @testable import VaporMySQL
 
 class VaporMySQL: XCTestCase {
-    static let allTests = []
+    static let allTests = [
+        ("testBasic", testBasic)
+    ]
+
+    func testBasic() {
+        XCTAssertEqual(2 + 2, 4)
+    }
 }

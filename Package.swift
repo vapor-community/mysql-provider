d88d62e46a3a7814a2ae3f341f32e7523cd4a7b3
Tanner Nelson <me@tanner.xyz>
2016-08-05 15:00:19 -0400

    vapor 16

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "VaporMySQL",
     dependencies: [
-   		.Package(url: "https://github.com/qutheory/fluent-mysql.git", majorVersion: 0, minor: 3),
-   		.Package(url: "https://github.com/qutheory/vapor.git", majorVersion: 0, minor: 15),
+   		.Package(url: "https://github.com/vapor/mysql-driver.git", majorVersion: 0, minor: 4),
+   		.Package(url: "https://github.com/vapor/vapor.git", majorVersion: 0, minor: 16),
     ]
 )

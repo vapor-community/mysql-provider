0bd81e1d1d4e003c23145ba715585b2c8305aef7
Tanner <me@tanner.xyz>
2017-02-17 17:19:30 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,10 +4,10 @@ let package = Package(
     name: "VaporMySQL",
     dependencies: [
         // MySQL driver for Fluent
-   		.Package(url: "https://github.com/vapor/mysql-driver.git", Version(2,0,0, prereleaseIdentifiers: ["alpha"])),
-   		// A provider for including Fluent in Vapor applications
+        .Package(url: "https://github.com/vapor/mysql-driver.git", Version(2,0,0, prereleaseIdentifiers: ["alpha"])),
+        // A provider for including Fluent in Vapor applications
         .Package(url: "https://github.com/vapor/fluent-provider.git", majorVersion: 0),
-   		// A web framework and server for Swift that works on macOS and Ubuntu.
-   		.Package(url: "https://github.com/vapor/vapor.git", Version(2,0,0, prereleaseIdentifiers: ["alpha"])),
+        // A web framework and server for Swift that works on macOS and Ubuntu.
+        .Package(url: "https://github.com/vapor/vapor.git", Version(2,0,0, prereleaseIdentifiers: ["alpha"])),
     ]
 )
